af8fcd16b24fad265665f46d751a0485bd393bbc
Alex McFarlane <alexander.mcfarlane@physics.org>
2016-03-18 00:57:07 +0000

    added significant testing proceedures and basic classes. Will implement into game in next commit.

--- config.py
+++ config.py
@@ -18,54 +18,24 @@ class Card(object):
     def get_money(self):
         return self.values[1]
     
-class GetConfig(object):
-    """Stores configuration paramters"""
+# separates classes in my editor
+class Central(object):
+    """The Central Deck Class"""
     def __init__(self):
-        """
-        Creates a static attribute to create
-        global variables.
-        """
+        """initial settings for the central cards"""
+        # create newgame paramters
+        self.newgame()
         pass
     
-    def initial(self):
-        """
-        Creates an dict.iteritem() of all initial game 
-        parameters.
-        
-        Warning: This will return every item instanced in
-        this function as it uses the local() method
-        
-        for item_name,item in self.initial:
-            globals()[item_name] = item
-        """
-        
-        # Initial settings
-        pO = { # User settings
-            'name': 'player one',
-            'health': 30,
-            'deck': None,
-            'hand': None,
-            'active': None,
-            'handsize': 5,
-            'discard': None}
-
-        pC = { # PC settings
-            'name': 'player computer',
-            'health': 30,
-            'deck': None,
-            'hand': None,
-            'active': None,
-            'handsize': 5,
-            'discard': None}
-
-        central = { # Central deck settings
-            'name': 'central',
-            'active': None,
-            'activesize': 5,
-            'supplement': None,
-            'deck': None}
-
-        sdc = [ # Central deck cards
+    def newgame(self):
+        self.central = { # Central deck settings
+                'name': 'central',
+                'active': None,
+                'activesize': 5,
+                'supplement': None,
+                'deck': None}
+    
+        self.sdc = [ # Central deck cards
                 4 * [Card('Archer', (3, 0), 2)],
                 4 * [Card('Baker', (0, 3), 2)],
                 3 * [Card('Swordsman', (4, 0), 3)],
@@ -77,92 +47,33 @@ class GetConfig(object):
                 4 * [Card('Thief', (1, 1), 1)],
                 2 * [Card('Catapult', (7, 0), 6)],
                 2 * [Card('Caravan', (1, 5), 5)],
-                2 * [Card('Assassin', (5, 0), 4)]
-            ]
-
-        # User's deck
-        playeronedeck = [
-            8 * [Card('Serf', (0, 1), 0)],
-            2 * [Card('Squire', (1, 0), 0)]
-            ]
-
-        # Flatten User deck into one list
-        pod = list(itertools.chain.from_iterable(playeronedeck))
-
-        # Initiate User deck dictionary
-        pO['deck'] = pod
-        pO['hand'] = []
-        pO['discard'] = []
-        pO['active'] = []
-
-        # Create User deck (list of lists)
-        playertwodeck = [
-            8 * [Card('Serf', (0, 1), 0)],
-            2 * [Card('Squire', (1, 0), 0)]
-            ]
-
-        # Flattens PC deck to one list
-        ptd = list(itertools.chain.from_iterable(playertwodeck))
-
-        # Initiate PC deck dictionary
-        pC['deck'] = ptd
-        pC['hand'] = []
-        pC['discard'] = []
-        pC['active'] = []
-
+                2 * [Card('Assassin', (5, 0), 4)]]
+        
         # Creating supplements
-        supplement = 10 * [Card('Levy', (1, 2), 2)]
-
+        self.supplement = 10 * [Card('Levy', (1, 2), 2)]
+        
         # Flatten central deck to one list
-        deck = list(itertools.chain.from_iterable(sdc))
-        random.shuffle(deck)
-
+        self.deck = list(itertools.chain.from_iterable(self.sdc))
+        random.shuffle(self.deck)
+        
         # Create central deck dictionary
-        central['deck'] = deck
-        central['supplement'] = supplement
-        central['active'] = []
-        
-        items = locals()
-        return items.iteritems()
+        self.central['deck'] = self.deck
+        self.central['supplement'] = self.supplement
+        self.central['active'] = []
+        pass
     
-    def newgame(self):
-        """
-        Creates an dict.iteritem() of all initial game 
-        parameters.
+    def replay(self):
+        """creates parameters for newgame"""
         
-        Warning: This will return every item instanced in
-        this function as it uses the local() method
-        
-        for item_name,item in self.initial:
-            globals()[item_name] = item
-        """
-        pO = { # User settings
-            'name': 'player one',
-            'health': 30,
-            'deck': None,
-            'hand': None,
-            'active': None,
-            'handsize': 5,
-            'discard': None}
-        
-        pC = {# PC settings
-            'name': 'player computer',
-            'health': 30,
-            'deck': None,
-            'hand': None,
-            'active': None,
-            'handsize': 5,
-            'discard': None}
-        
-        central = { # Central deck settings
+        self.central = { # Central deck settings
             'name': 'central',
             'active': None,
             'activesize': 5,
             'supplement': None,
             'deck': None}
-        
-         # Changes are made in this list
-        sdc = [ # Central deck cards
+            
+        # Changes are made in this list
+        self.sdc = [ # Central deck cards
             4 * [Card('Archer', (3, 0), 2)],
             4 * [Card('Baker', (0, 3), 2)],
             3 * [Card('Swordsman', (4, 0), 3)],
@@ -174,51 +85,140 @@ class GetConfig(object):
             4 * [Card('Thief', (1, 1), 1)],
             2 * [Card('Catapault', (7, 0), 6)],
             2 * [Card('Caravan', (1, 5), 5)],
-            2 * [Card('Assassin', (5, 0), 4)]
-        ]
+            2 * [Card('Assassin', (5, 0), 4)]]
+        
+        # Creating supplements 
+        self.supplement = 10 * [Card('Levy', (1, 2), 2)]
+        
+        # Flatten central deck to one list
+        self.deck = list(itertools.chain.from_iterable(self.sdc))
+        random.shuffle(self.deck)
+        
+        # Initiate PC deck dictionary
+        self.central['deck'] = self.deck
+        self.central['supplement'] = self.supplement
+        self.central['active'] = []
+        pass
+
+# separates classes in my editor
+class User(object):
+    """The User Class"""
+    def __init__(self):
+        """initial settings for the User"""
+        # create newgame paramters
+        self.newgame()
+        pass
+    
+    def newgame(self):
+        """initial default settings for the user"""
+        # Initial settings
+        self.pO = { # User settings
+            'name': 'player one',
+            'health': 30,
+            'deck': None,
+            'hand': None,
+            'active': None,
+            'handsize': 5,
+            'discard': None}
+        
+        self.playeronedeck = [ # User's deck
+            8 * [Card('Serf', (0, 1), 0)],
+            2 * [Card('Squire', (1, 0), 0)]
+            ]
+        
+        # Flatten User deck into one list
+        self.pod = list(itertools.chain.from_iterable(self.playeronedeck))
+        
+        # Initiate User deck dictionary
+        self.pO['deck'] = self.pod
+        self.pO['hand'] = []
+        self.pO['discard'] = []
+        self.pO['active'] = []
+        pass
+    
+    def replay(self):
+        """creates parameters for newgame"""
+        self.pO = { # User settings
+            'name': 'player one',
+            'health': 30,
+            'deck': None,
+            'hand': None,
+            'active': None,
+            'handsize': 5,
+            'discard': None}
         
         # Changes are made in this list
         # User's deck
-        playeronedeck = [8 * [Card('Serf', (0, 1), 0)],
-                         2 * [Card('Squire', (1, 0), 0)]
-                    ]
+        self.playeronedeck = [
+            8 * [Card('Serf', (0, 1), 0)],
+            2 * [Card('Squire', (1, 0), 0)]]
         
         # Flatten list to one list
-        pod = list(itertools.chain.from_iterable(playeronedeck))
+        self.pod = list(itertools.chain.from_iterable(self.playeronedeck))
         
         # Initiate new dictionary
-        pO['deck'] = pod
-        pO['hand'] = []
-        pO['discard'] = []
-        pO['active'] = []
+        self.pO['deck'] = self.pod
+        self.pO['hand'] = []
+        self.pO['discard'] = []
+        self.pO['active'] = []
+
+# separates classes in my editor
+class Computer(object):
+    """The Computer Player Class"""
+    def __init__(self):
+        """initial settings for the computer player"""
+        
+        # create newgame paramters
+        self.newgame()
+        
+    def newgame(self):
+        self.pC = { # PC settings
+            'name': 'player computer',
+            'health': 30,
+            'deck': None,
+            'hand': None,
+            'active': None,
+            'handsize': 5,
+            'discard': None}
+        
+        # Create User deck (list of lists)
+        self.playertwodeck = [
+            8 * [Card('Serf', (0, 1), 0)],
+            2 * [Card('Squire', (1, 0), 0)]]
+            
+        # Flattens PC deck to one list
+        self.ptd = list(itertools.chain.from_iterable(self.playertwodeck))
+        
+        # Initiate PC deck dictionary
+        self.pC['deck'] = self.ptd
+        self.pC['hand'] = []
+        self.pC['discard'] = []
+        self.pC['active'] = []
+        pass
+    
+    def replay(self):
+        """creates parameters for newgame"""
+        
+        self.pC = {# PC settings
+            'name': 'player computer',
+            'health': 30,
+            'deck': None,
+            'hand': None,
+            'active': None,
+            'handsize': 5,
+            'discard': None}
         
         # Changes are made in this list
         # Create PC deck (list of lists)
-        playertwodeck = [
-                    8 * [Card('Serf', (0, 1), 0)],
-                    2 * [Card('Squire', (1, 0), 0)]
-        ]
+        self.playertwodeck = [
+            8 * [Card('Serf', (0, 1), 0)],
+            2 * [Card('Squire', (1, 0), 0)]]
         
         # Flatten list of lists
-        ptd = list(itertools.chain.from_iterable(playertwodeck))
+        self.ptd = list(itertools.chain.from_iterable(self.playertwodeck))
         
         # Initiate PC deck dictionary
-        pC['deck'] = ptd
-        pC['hand'] = []
-        pC['discard'] = []
-        pC['active'] = []
-        
-        # Creating supplements 
-        supplement = 10 * [Card('Levy', (1, 2), 2)]
-        
-        # Flatten central deck to one list
-        deck = list(itertools.chain.from_iterable(sdc))
-        random.shuffle(deck)
-        
-        # Initiate PC deck dictionary
-        central['deck'] = deck
-        central['supplement'] = supplement
-        central['active'] = []
-        
-        items = locals()
-        return items.iteritems()
+        self.pC['deck'] = self.ptd
+        self.pC['hand'] = []
+        self.pC['discard'] = []
+        self.pC['active'] = []
--- test.py
+++ test.py
@@ -1,29 +1,372 @@
 # This is a python testing file
 from config import *
 import itertools, random
+import collections
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+def flatten(l):
+    """This flattens arbitrarily annoying lists / dicts"""
+    for el in l:
+        if isinstance(el, collections.Iterable) and not isinstance(el, basestring):
+            for sub in flatten(el):
+                yield sub
+        else:
+            yield el
 
 class Settings(object):
+    """This contains the tests related to the game settings"""
     def __init__(self):
         """run tests"""
-        self.runAll()
+        
+        # creates a logger for the test file
+        self.logger = logging.getLogger("Settings")
+        self.logger.info('Logger started.')
+        
+        # create classes of users and central deck
+        self.central = Central()
+        self.user = User()
+        self.computer = Computer()
+        
+        # runs all tests
+        tests = [
+            "newgame_settings",
+            "replay_settings"]
+        
+        self.run_tests(tests)
         pass
     
-    def runAll(self, display=True):
+    def run_tests(self, tests, display=True):
         """Runs all functions"""
-        self.all_tests = ["importing"]
         
         # loop through all functions
         # and get results - store in a dictionary
         # prints out if display=True
-        for func_name in self.all_tests:
+        for func_name in tests:
+            self.logger.info("Running Test: {}".format(func_name))
             res = getattr(self, func_name)()
-            if display: print "Test: {} - {}".format(func_name, res)
+            
+            if res: 
+                self.logger.error("Test Failed: {}".format(func_name)) 
+            else:
+                self.logger.info("Test Passed: {}".format(func_name))
         
         pass
     
-    def importing(self):
-        """tests that the class is imported as expected"""
+    def newgame_settings(self):
+        """Tests that new game settings match the original game settings"""
         
+        self.computer.newgame()
+        self.user.newgame()
+        self.central.newgame()
+        
+        test_attrs = {
+            'computer':self.computer.__dict__,
+            'user':self.user.__dict__,
+            'central':self.central.__dict__}
+        
+        
+        # loop through all variables
+        original_settings = self.__original_newgame_settings()
+        error = self.__loop_compare(original_settings, test_attrs)
+        return error
+    
+    def replay_settings(self):
+        """Tests that replay game settings match the original game settings"""
+        
+        # create parameters for tests
+        self.computer.replay()
+        self.user.replay()
+        self.central.replay()
+        
+        test_attrs = {
+            'computer':self.computer.__dict__,
+            'user':self.user.__dict__,
+            'central':self.central.__dict__}
+        
+        # loop through all variables
+        original_settings = self.__original_replay_settings()
+        error = self.__loop_compare(original_settings, test_attrs)
+        return error
+    
+    def __loop_compare(self, original_settings, test_attrs):
+        """combined functionality for initial settings comparision
+        
+        Used in:
+            self.replay_settings()
+            self.newgame_settings()
+            
+        compares values from the two input lists
+        """
+        
+        error = False
+                
+        for item_name,item in original_settings.iteritems():
+            if item_name == 'self': continue # don't compare self!
+            # loop through each class and find the class that contains
+            # the variable - check it is correct and verify vairables
+            in_class = None
+            attr_is_same = False
+            for class_name,class_attrs in test_attrs.iteritems():
+                
+                # see if variable is in class
+                if item_name in class_attrs.keys():
+                    in_class = class_name
+                    
+                    if in_class: 
+                        attr_is_same = self.compare_vals(class_attrs[item_name], item)
+                        
+            
+            # report error status
+            if in_class:
+                
+                if attr_is_same: 
+                    self.logger.debug("Items are same. Found {0} in {1}".format(
+                        item_name, in_class, attr_is_same))
+                else:
+                    error = True
+                    self.logger.warning("Items not same. Found {0} in {1}".format(
+                        item_name, in_class, attr_is_same))
+            else:
+                print "Couldn't find attr: {}".format(item_name)
+                error=True
+        return error
+    
+    def compare_vals(self, a, b):
+        """Compares values from the game
+        
+        Input::
+        a,b :: Iterables - recursive allowed can contain Card()
+        
+        Output::
+        same :: boolean - True if items seem the same
+        
+        Creates a list of flattened parameters using 
+        the function flattened()
+        
+        This returns most values but Card() remain.
+        Fundamentally, Card() contain the parameters we
+        need to check so an isinstance() and __dict__ rip
+        open each and every Card()'s attributes
+        
+        We then compare sorted lists.
+        """
+        
+        same = False
+        
+        # create unsorted flattened lists for a and b
+        
+        a = [i.__dict__ if isinstance(i,Card) else i \
+            for i in flatten(a)]
+        
+        b = [i.__dict__ if isinstance(i,Card) else i \
+            for i in flatten(b)]
+        
+        # must sort the variables to ensure correct comparision
+        a = sorted(a)
+        b = sorted(b)
+        
+        # are both items the same
+        same = (b == a)
+        return same
+    def __original_newgame_settings(self):
+        """
+        Creates an dict.iteritem() of all initial game 
+        parameters.
+    
+        Warning: This will return every item instanced in
+        this function as it uses the local() method
+    
+        for item_name,item in self.initial:
+            globals()[item_name] = item
+        """
+    
+        # Initial settings
+        pO = { # User settings
+            'name': 'player one',
+            'health': 30,
+            'deck': None,
+            'hand': None,
+            'active': None,
+            'handsize': 5,
+            'discard': None}
+
+        pC = { # PC settings
+            'name': 'player computer',
+            'health': 30,
+            'deck': None,
+            'hand': None,
+            'active': None,
+            'handsize': 5,
+            'discard': None}
+
+        central = { # Central deck settings
+            'name': 'central',
+            'active': None,
+            'activesize': 5,
+            'supplement': None,
+            'deck': None}
+
+        sdc = [ # Central deck cards
+                4 * [Card('Archer', (3, 0), 2)],
+                4 * [Card('Baker', (0, 3), 2)],
+                3 * [Card('Swordsman', (4, 0), 3)],
+                2 * [Card('Knight', (6, 0), 5)],
+                3 * [Card('Tailor', (0, 4), 3)],
+                3 * [Card('Crossbowman', (4, 0), 3)],
+                3 * [Card('Merchant', (0, 5), 4)],
+                4 * [Card('Thug', (2, 0), 1)],
+                4 * [Card('Thief', (1, 1), 1)],
+                2 * [Card('Catapult', (7, 0), 6)],
+                2 * [Card('Caravan', (1, 5), 5)],
+                2 * [Card('Assassin', (5, 0), 4)]
+            ]
+
+        # User's deck
+        playeronedeck = [
+            8 * [Card('Serf', (0, 1), 0)],
+            2 * [Card('Squire', (1, 0), 0)]
+            ]
+
+        # Flatten User deck into one list
+        pod = list(itertools.chain.from_iterable(playeronedeck))
+
+        # Initiate User deck dictionary
+        pO['deck'] = pod
+        pO['hand'] = []
+        pO['discard'] = []
+        pO['active'] = []
+
+        # Create User deck (list of lists)
+        playertwodeck = [
+            8 * [Card('Serf', (0, 1), 0)],
+            2 * [Card('Squire', (1, 0), 0)]
+            ]
+
+        # Flattens PC deck to one list
+        ptd = list(itertools.chain.from_iterable(playertwodeck))
+
+        # Initiate PC deck dictionary
+        pC['deck'] = ptd
+        pC['hand'] = []
+        pC['discard'] = []
+        pC['active'] = []
+
+        # Creating supplements
+        supplement = 10 * [Card('Levy', (1, 2), 2)]
+
+        # Flatten central deck to one list
+        deck = list(itertools.chain.from_iterable(sdc))
+        random.shuffle(deck)
+
+        # Create central deck dictionary
+        central['deck'] = deck
+        central['supplement'] = supplement
+        central['active'] = []
+    
+        items = locals()
+        return items
+
+    def __original_replay_settings(self):
+        """
+        Creates an dict.iteritem() of all initial game 
+        parameters.
+    
+        Warning: This will return every item instanced in
+        this function as it uses the local() method
+    
+        for item_name,item in self.initial:
+            globals()[item_name] = item
+        """
+        pO = { # User settings
+            'name': 'player one',
+            'health': 30,
+            'deck': None,
+            'hand': None,
+            'active': None,
+            'handsize': 5,
+            'discard': None}
+    
+        pC = {# PC settings
+            'name': 'player computer',
+            'health': 30,
+            'deck': None,
+            'hand': None,
+            'active': None,
+            'handsize': 5,
+            'discard': None}
+    
+        central = { # Central deck settings
+            'name': 'central',
+            'active': None,
+            'activesize': 5,
+            'supplement': None,
+            'deck': None}
+    
+         # Changes are made in this list
+        sdc = [ # Central deck cards
+            4 * [Card('Archer', (3, 0), 2)],
+            4 * [Card('Baker', (0, 3), 2)],
+            3 * [Card('Swordsman', (4, 0), 3)],
+            2 * [Card('Knight', (6, 0), 5)],
+            3 * [Card('Tailor', (0, 4), 3)],
+            3 * [Card('Crossbowman', (4, 0), 3)],
+            3 * [Card('Merchant', (0, 5), 4)],
+            4 * [Card('Thug', (2, 0), 1)],
+            4 * [Card('Thief', (1, 1), 1)],
+            2 * [Card('Catapault', (7, 0), 6)],
+            2 * [Card('Caravan', (1, 5), 5)],
+            2 * [Card('Assassin', (5, 0), 4)]
+        ]
+    
+        # Changes are made in this list
+        # User's deck
+        playeronedeck = [8 * [Card('Serf', (0, 1), 0)],
+                         2 * [Card('Squire', (1, 0), 0)]
+                    ]
+    
+        # Flatten list to one list
+        pod = list(itertools.chain.from_iterable(playeronedeck))
+    
+        # Initiate new dictionary
+        pO['deck'] = pod
+        pO['hand'] = []
+        pO['discard'] = []
+        pO['active'] = []
+    
+        # Changes are made in this list
+        # Create PC deck (list of lists)
+        playertwodeck = [
+                    8 * [Card('Serf', (0, 1), 0)],
+                    2 * [Card('Squire', (1, 0), 0)]
+        ]
+    
+        # Flatten list of lists
+        ptd = list(itertools.chain.from_iterable(playertwodeck))
+    
+        # Initiate PC deck dictionary
+        pC['deck'] = ptd
+        pC['hand'] = []
+        pC['discard'] = []
+        pC['active'] = []
+    
+        # Creating supplements 
+        supplement = 10 * [Card('Levy', (1, 2), 2)]
+    
+        # Flatten central deck to one list
+        deck = list(itertools.chain.from_iterable(sdc))
+        random.shuffle(deck)
+    
+        # Initiate PC deck dictionary
+        central['deck'] = deck
+        central['supplement'] = supplement
+        central['active'] = []
+    
+        items = locals()
+        return items
+    
+    def __redundant__importing(self):
+        """tests that the class is imported as expected"""
         
         # Attempt to obtain a Card object
         test = Card('Archer', (3, 0), 2)
@@ -47,6 +390,7 @@ class Settings(object):
         
         # will only return if all assertions succeed
         return True
+        
 
 if __name__ == '__main__':
     test = Settings()
